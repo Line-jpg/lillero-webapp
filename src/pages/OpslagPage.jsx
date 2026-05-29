b8bd0600d71277f8ce134df5a645114c33a680e5
Line-jpg <lines1310@gmail.com>
2026-05-29 12:43:27 +0200

    Justeringer i CSS

--- src/pages/OpslagPage.jsx
+++ src/pages/OpslagPage.jsx
@@ -1,6 +1,12 @@
+import Header from "../components/Header";
 import Posts from "../components/opslag";
 import "../opslag.css";
 
 export default function AboutPage() {
-  return <Posts />;
+  return (
+    <>
+      <Header />
+      <Posts showTitle={false} />
+    </>
+  );
 }
